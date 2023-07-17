11518e28e317c945a6a8d455f6a6d387a3bcbd94
Raul Conchello Vendrell <raulconchello@gmail.com>
2023-07-17 13:31:19 -0400

    new layers

--- my_code/layers.py
+++ my_code/layers.py
@@ -60,26 +60,33 @@ class layer:
 class basis_preparation(layer):
     statepreparation_layer = True
 
-    def gates(self, input):
+    @property
+    def len_input(self):
+        return len(self.qubits)
+    
+    @property
+    def mock_input(self):
+        return np.random.rand(self.len_input)
+    
+    def _check_input(self, input):
         if len(input) != len(self.qubits):
-            raise Exception('basis_preparation layer: Invalid input size. Data must have the correct input size. In this case, qubits="{}", which means that len(input) should be {}.'.format(self._qubits, len(self.qubits)))
+            raise Exception('State preparation layer: Invalid input size. Data must have the correct input size. In this case, qubits="{}", which means that len(input) should be {}.'.format(self._qubits, len(self.qubits)))
         
+    def gates(self, input):
+        self._check_input(input)
         qml.BasisState(input, wires=self.qubits)
 
-class angle_preparation(layer):
-    statepreparation_layer = True
+class angle_preparation(basis_preparation):
 
     def gates(self, input):
-        if len(input) != len(self.qubits):
-            raise Exception('angle_preparation layer: Invalid input size. Data must have the correct input size. In this case, qubits="{}", which means that len(input) should be {}.'.format(self._qubits, len(self.qubits)))
-        
+        self._check_input(input)
         qml.AngleEmbedding(input, wires=self.qubits)
 
 # fixed layer classes
 class CNOTs_layer(layer):
 
     def gates(self):
-        n_qubits = self.n_qubits
+        n_qubits = len(self.qubits)
         qubits = self.qubits
 
         for i in range(n_qubits): # entangle qubits: qubits 0 and 1 are entangled with CNOT, qubits 2 and 3 are entangled with CNOT, ..., qubits n_wires-1 and 0 are entangled with CNOT
@@ -96,10 +103,53 @@ class rotation_layer(layer):
         for i, q in enumerate(self.qubits): # rotate differently each qubit (each rotation needs 3 parameters)
             qml.Rot(params[i, 0], params[i, 1], params[i, 2], wires=q)
 
-        # #CNOTs between all qubits
-        # for i in range(n_wires):
-        #     for j in range(i+1,n_wires):
-        #         qml.CNOT(wires=[i,j])
+class rotationX_layer(layer):
+
+    @property
+    def shape_params(self):
+        return (len(self.qubits), 1)
+
+    def gates(self, params):
+        for i, q in enumerate(self.qubits): # X rotation for each qubit
+            qml.RX(params[i,0], wires=q)
+
+class rotationZ_layer(rotationX_layer):
+
+    def gates(self, params):
+        for i, q in enumerate(self.qubits): # Z rotation for each qubit
+            qml.RZ(params[i,0], wires=q)
+
+class rotationY_layer(rotationX_layer):
+    
+    def gates(self, params):
+        for i, q in enumerate(self.qubits): # Y rotation for each qubit
+            qml.RY(params[i,0], wires=q)
+
+class rotationZZ_layer(layer):
+    
+        @property
+        def shape_params(self):
+            return (len(self.qubits), 1)
+    
+        def gates(self, params):
+            qubits = self.qubits
+            n_qubits = len(qubits)
+
+            for i in range(0,len(qubits),2): # ZZ rotation for neighboring qubits (qubits 0 and 1, qubits 2 and 3, ..., qubits n_wires-1 and 0)
+
+                ## ZZ rotation between qubits i and i+1
+                qml.CNOT(wires=[qubits[i], qubits[(i+1)%n_qubits]])
+                qml.RZ(params[i,0], wires=qubits[(i+1)%n_qubits])
+                qml.CNOT(wires=[qubits[i], qubits[(i+1)%n_qubits]])
+
+            for i in range(1,len(qubits),2): # ZZ rotation for neighboring qubits (qubits 1 and 2, qubits 3 and 4, ..., qubits n_wires-2 and n_wires-1)
+
+                ## ZZ rotation between qubits i and i+1
+                qml.CNOT(wires=[qubits[i], qubits[(i+1)%n_qubits]])
+                qml.RZ(params[i,0], wires=qubits[(i+1)%n_qubits])
+                qml.CNOT(wires=[qubits[i], qubits[(i+1)%n_qubits]])
+
+        
 
 # measurement layer classes
 class mesurament(layer):
