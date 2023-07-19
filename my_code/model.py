64fe9d79c4544dcbfc223c7bbf04e817b5b02ca1
Raul Conchello Vendrell <raulconchello@gmail.com>
2023-07-19 14:26:45 -0400

    changes in the model class

--- my_code/model.py
+++ my_code/model.py
@@ -20,17 +20,30 @@ class model:
             output_to_prediction = get_value
         ):
 
-        # number of qubits and device
-        self.n_qubits = n_qubits_data
-        self.dev = qml.device(device, wires=n_qubits_data)
-
         # circuit_layers
         self.circuit_layers = circuit_layers
-        for layer in self.circuit_layers: layer.n_qubits = self.n_qubits # set the number of qubits for each layer
+
+        # set the number of qubits for each layer
+        for layer in self.circuit_layers: layer.n_qubits_circuit = n_qubits_data
+
+        # set the number of ancillas for each layer
+        n_ancillas = 0
+        for layer in self.circuit_layers: 
+            if getattr(layer, 'ancilla_layer', False) == True:
+                n_ancillas = layer.n_ancillas if layer.n_ancillas > n_ancillas else n_ancillas
+
+        for layer in self.circuit_layers: layer.n_ancillas_circuit = n_ancillas 
+
+        # check if the first and last layers are state preparation and mesurament layers
         if self.circuit_layers[0].statepreparation_layer == False:
             raise Exception('The first circuit layer must be a state preparation layer.')
-        if self.circuit_layers[-1].mesuarment_layer == False:
-            raise Exception('The last circuit layer must be a mesuarment layer.')
+        if self.circuit_layers[-1].mesurament_layer == False:
+            raise Exception('The last circuit layer must be a mesurament layer.')
+        
+        # number of qubits and device
+        self.n_qubits = n_qubits_data
+        self.n_ancillas = n_ancillas
+        self.dev = qml.device(device, wires=range(-1*n_ancillas,n_qubits_data))
 
         # parameters and bias, and training records
         self.bias = bias
@@ -39,6 +52,9 @@ class model:
 
         # output to prediction function
         self.output_to_prediction = output_to_prediction
+
+
+        self.times = 0
 
     def set_data(self, data_X, data_Y, data_X_validation=None, data_Y_validation=None):
 
@@ -65,7 +81,9 @@ class model:
         self.accuracies_batch = []
         self.accuracies_validation = []
 
-    def variational_classifier(self, input, params, draw=False, draw_options={}):
+    def parametric_model(self, input, params, draw=False, draw_options={}):
+
+        self.times += 1
 
         @qml.qnode(self.dev, interface="autograd")
         def circuit(input, params):
@@ -97,7 +115,7 @@ class model:
     
     def draw_circuit(self, size=(6, 3)):
         draw_options = {'size': size}
-        self.variational_classifier(self.circuit_layers[0].mock_input, self.params, draw=True, draw_options=draw_options)
+        self.parametric_model(self.circuit_layers[0].mock_input, self.params, draw=True, draw_options=draw_options)
     
     def loss(self, labels, predictions): # squared loss
         loss = 0
@@ -106,12 +124,12 @@ class model:
         loss = loss / len(labels)
         return loss
     
-    def accuracy(self, objectives, predictions):
-        loss = self.loss(objectives, predictions)
-        return 1 - loss
+    # def accuracy(self, objectives, predictions):
+    #     loss = self.loss(objectives, predictions)
+    #     return 1 - loss
     
     def cost(self, X, Y, *params):
-        output = [self.variational_classifier(x, params) for x in X]
+        output = [self.parametric_model(x, params) for x in X]
         cost = self.loss(Y, output)
         self.last_cost = { #TODO history of costs and parameters
             'X': X,
@@ -124,12 +142,15 @@ class model:
                 self,  
                 epochs = 10, 
                 optimizer = qml.GradientDescentOptimizer(),
+                cost_every = 1,
                 batch_size = 10,
                 randomize_batches = False,
                 initialize_params = False, 
                 plot=True, 
                 plot_options={}
             ):
+        
+        self.times = 0
         
         # set optimizer and batch size
         self.optimizer = optimizer
@@ -163,44 +184,55 @@ class model:
                 X_batch = data_X_batches[it]
                 Y_batch = data_Y_batches[it]                
 
-                # Update parameters and append cost
-                params, cost = self.optimizer.step_and_cost(self.cost, X_batch, Y_batch, *self.params)
-                self.params = params[2:]
+                # Update parameters and append cost (if optimizer is SPSA, we compute cost every 'x' iterations, where 'x' is cost_every)
+                if not isinstance(self.optimizer, qml.SPSAOptimizer) or it % cost_every == 0:
+                    params, cost = self.optimizer.step_and_cost(self.cost, X_batch, Y_batch, *self.params)
+                    self.params = params[2:]
+                    print("Epoch: {} | Iter: {:5d} | Cost: {:0.7f}".format(epoch, it + 1, cost))
+                else:
+                    self.params, cost = self.optimizer.step(self.cost, X_batch, Y_batch, *self.params)[2:], None
+                    print("Epoch: {} | Iter: {:5d}".format(epoch, it + 1))
                 self.costs.append(cost)
-
-                # Compute accuracy batch
-                predictions = [self.output_to_prediction(x) for x in self.last_cost['output']]
-                acc = self.accuracy(Y_batch, predictions)
-                self.accuracies_batch.append(acc)
 
                 # plot progress
                 if plot: self.plot(iteration=it, last_iteration=it==(iterations-1), **plot_options)
 
-                # print progress
-                if self.data_validation_X is not None:
-                    # Compute accuracy validation
-                    predictions = [self.output_to_prediction(self.variational_classifier(x, self.params)) for x in self.data_validation_X]
-                    acc_val = self.accuracy(self.data_validation_Y, predictions)
-                    self.accuracies_validation.append(acc_val)
 
-                    # Print progress (cost, accuracy batch and accuracy validation for this batch)
-                    print("Epoch: {} | Iter: {:5d} | Cost: {:0.7f} | Accuracy: {:0.7f} | Accuracy validation: {:0.7f}".format(epoch, it + 1, cost, acc, acc_val))
 
-                else:
-                    # Print progress (cost and accuracy for this batch)
-                    print("Epoch: {} | Iter: {:5d} | Cost: {:0.7f} | Accuracy: {:0.7f} ".format(epoch, it + 1, cost, acc))
+                # # Compute accuracy batch
+                # predictions = [self.output_to_prediction(x) for x in self.last_cost['output']]
+                # acc = self.accuracy(Y_batch, predictions)
+                # self.accuracies_batch.append(acc)
+
+                # 
+                #     if self.data_validation_X is not None:
+                #         # Compute accuracy validation
+                #         predictions = [self.output_to_prediction(self.parametric_model(x, self.params)) for x in self.data_validation_X]
+                #         acc_val = self.accuracy(self.data_validation_Y, predictions)
+                #         self.accuracies_validation.append(acc_val)
+
+                #         # Print progress (cost, accuracy batch and accuracy validation for this batch)
+                #         print("Epoch: {} | Iter: {:5d} | Cost: {:0.7f} | Accuracy: {:0.7f} | Accuracy validation: {:0.7f}".format(epoch, it + 1, cost, acc, acc_val))
+
+                #     else:
+                #         # Print progress (cost and accuracy for this batch)
+                #         cost = self.cost(X_batch, Y_batch, *self.params)
+
+                #         print("Epoch: {} | Iter: {:5d} | Cost: {:0.7f} | Accuracy: {:0.7f} ".format(epoch, it + 1, cost, acc))
+
+                
 
             
 
     def predict(self, input):
-        return self.output_to_prediction(self.variational_classifier(input, self.params))
+        return self.output_to_prediction(self.parametric_model(input, self.params))
 
     def plot(self, iteration, last_iteration=False, cost=True, accuracy=True, accuracy_validation=True, plot_every=1):
 
         if (iteration+1) % plot_every == 0 or last_iteration:
             
             # Clear previous iteration outputs
-            clear_output(wait=True)
+            # clear_output(wait=True)
 
             # Create a new figure
             fig, ax1 = plt.subplots()
@@ -208,9 +240,11 @@ class model:
             # Create a second y-axis that shares the same x-axis
             ax2 = ax1.twinx()
 
+            costs = [c for c in self.costs if c is not None]
+
             if cost:
                 # Plot cost on the first y-axis
-                ax1.plot(self.costs, 'g-')
+                ax1.plot(costs, 'g-')
                 ax1.set_xlabel('Iteration')
                 ax1.set_ylabel('Cost', color='g')
                 ax1.tick_params('y', colors='g')
