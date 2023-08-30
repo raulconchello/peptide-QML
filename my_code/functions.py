77c851ea4c4fb523078cfc852b2800d939afa73e
Raul Conchello Vendrell <raulconchello@gmail.com>
2023-08-30 13:51:33 -0400

    string_to_single_energy

--- my_code/functions.py
+++ my_code/functions.py
@@ -118,8 +118,12 @@ def string_to_energy(string, h, J):
 
     return energy.item()
 
+def string_to_single_energy(string, h):    
+    vector = string_to_numbers(string)
+    energy = np.sum(h[np.arange(len(vector)), vector])
+    return energy.item()
 
-def generate_random_data_from_energies(file_single_path, file_pair_path, file_out_path, max_score=None, n_samples=1000, n_amino_acids=12):
+def generate_random_data_from_energies(file_single_path, file_pair_path, file_out_path, max_score=None, n_samples=1000, n_amino_acids=12, only_single_energies=False):
 
     h, J = read_energies_file(file_single_path, file_pair_path)
 
@@ -130,7 +134,7 @@ def generate_random_data_from_energies(file_single_path, file_pair_path, file_ou
 
         while n_samples < n_samples_to_do:
             string = ''.join(random.choice(POSSIBLE_AMINOACIDS_LETTER) for _ in range(n_amino_acids))
-            score = string_to_energy(string, h, J)
+            score = string_to_energy(string, h, J) if not only_single_energies else string_to_single_energy(string, h)
 
             if max_score is None or score <= max_score:
                 file.write(f'{string}  {score}\n')
